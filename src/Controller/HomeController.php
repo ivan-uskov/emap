dbba2ef3c46cd565e473d38dfeaacfff43a22e96
Ivan Uskov <ivan.uskov@cpslabs.net>
2020-02-29 23:13:33 +0300

    Добавил шаблонизатор

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,10 +9,8 @@ class HomeController extends AbstractController
 {
     public function home(): Response
     {
-        $number = random_int(0, 100);
-
         return $this->render('home.html.twig', [
-            'number' => $number,
+
         ]);
     }
 }
