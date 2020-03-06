f88e8baa0db6f728c60436e6da16652c487493f7
Ivan Uskov <ivan.uskov@cpslabs.net>
2020-03-06 23:54:27 +0300

    Добавил ajax controller

--- src/Controller/MelogramController.php
+++ src/Controller/MelogramController.php
@@ -6,6 +6,8 @@ use App\Module\Emap\Api\Api;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class MelogramController extends AbstractController
 {
     public function page(): Response
@@ -17,6 +19,14 @@ class MelogramController extends AbstractController
 
     public function ajax(): Response
     {
+        $request = Request::createFromGlobals();
+        if ($request->getMethod() !== Request::METHOD_POST)
+        {
+            return new Response("Not Found",404);
+        }
+
+        $melogramName = (string) $request->query->get('melogram_name');
+
         $api = new Api($this->getDoctrine());
 
         return $this->render('melogram.html.twig', ['hierarchy' => $api->getHierarchyVariantsList()->asAssoc()]);
