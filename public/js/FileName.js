757fab13e86cf23f1ac3014370c10c0619910bde
elena-zaharova <elenazahar1998@gmail.com>
2020-03-31 18:17:17 +0300

    создала шаблоны для выборок

---
+++ public/js/FileName.js
@@ -0,0 +1,8 @@
+$(function () {
+    console.log("Hello");
+    document.getElementById('inputGroupFile01').onchange = function () {
+        var file = document.getElementById('inputGroupFile01').value.replace(/.*[\\\/]/, "");
+        var placeFile = document.getElementById('inputGroupFile01').nextElementSibling;
+        placeFile.innerHTML = file;
+    }
+})
