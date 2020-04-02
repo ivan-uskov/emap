95b3b6cfee175e2bb0f05c546bf089c5dd64927f
elena-zaharova <elenazahar1998@gmail.com>
2020-04-02 09:39:10 +0300

    добавила отображение загружаемого миди файла

--- public/js/FileName.js
+++ public/js/FileName.js
@@ -1,5 +1,4 @@
 $(function () {
-    console.log("Hello");
     document.getElementById('inputGroupFile01').onchange = function () {
         var file = document.getElementById('inputGroupFile01').value.replace(/.*[\\\/]/, "");
         var placeFile = document.getElementById('inputGroupFile01').nextElementSibling;
