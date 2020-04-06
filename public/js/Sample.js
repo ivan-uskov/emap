00c574836de84f7936c6a6b71c45becb9be50918
elena-zaharova <elenazahar1998@gmail.com>
2020-04-06 17:37:12 +0300

    внесла правки

--- public/js/Sample.js
+++ public/js/Sample.js
@@ -1,6 +1,16 @@
-$(function () {
+$(document).ready(function () {
+    var count = 0;
     $('#add_row_link').click(function() {
-        var elem  = $('#empty_row').clone();
+        var elem  = $('#empty_row').clone(true);
         $(elem).appendTo('tbody');
+        ++count;
     });
+    $('.remove_row_link').click(function () {
+        if(count >= 1){
+            var th = $(this).parent();
+            var td = $(th).parent();
+            td.remove();
+            --count;
+        }
+    })
 });
