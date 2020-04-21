6b4ebab3d9f2f9e0acf9fc226180fca8c5656487
Ivan Uskov <ivan.uskov@ispringsolutions.com>
2020-04-21 13:00:01 +0300

    Добавил страницы

---
+++ src/Controller/SelectionGroupController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Module\Emap\Api\Api;
+use App\Module\Emap\Api\ApiInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SelectionGroupController extends AbstractController
+{
+    public function add(): Response
+    {
+
+    }
+
+    public function list(): Response
+    {
+
+    }
+
+    public function remove(int $id): Response
+    {
+
+    }
+
+    public function view(int $id): Response
+    {
+
+    }
+}
